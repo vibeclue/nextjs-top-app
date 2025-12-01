0344e9be20192491bd9603743096f43cd598b7b7
vibeclue <most.possible@mail.ru>
2025-12-01 10:33:14 +0300

    добавил правила eslint для хуков (но не работает), что бы ругаться когда пишу не так.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,22 @@
+'use client'
 import { Button, Htag, Ptag,Tag } from "@/components";
+import { useEffect, useState } from "react";
 
 // ФИГМА: https://www.figma.com/design/Cy3uqi4SSRfk89hLq85lbN/%D0%9A%D1%83%D1%80%D1%81-2---NextJS?node-id=1-2&t=lGdd2H9HOXI1SZRr-0
 export default function Home() {
 
+  const [counter, setCounter] = useState<number>(0);
+  
+
+    useEffect(() => {
+      console.log(`Состояние counter изменилось, теперь оно ${counter}`);
+    }, [counter]);
+
+
   return (
     <>
-      <Htag tag='h1'>Текст</Htag>
-      <Button appearance='primary' arrow='right'>Кнопка</Button>
+      <Htag tag='h1'>{counter}</Htag>
+      <Button appearance='primary' arrow='right' onClick={() => setCounter(counter+1)}>Кнопка</Button>
       <Button appearance='ghost' arrow='down'>Кнопка</Button>
       <Ptag size='s'>Малый: Тут хотелосб бы видеть очень мног отекста ванпаовоавк ак ваыва выа </Ptag>
       <Ptag size='m'>Средний: Тут хотелосб бы видеть очень мног отекста ванпаовоавк ак ваыва выа </Ptag>
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import reactHooks from 'eslint-plugin-react-hooks';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +14,33 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {    
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    plugins: {
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      // React Compiler rules
+      'react-hooks/config': 'error',
+      'react-hooks/error-boundaries': 'error',
+      'react-hooks/component-hook-factories': 'error',
+      'react-hooks/gating': 'error',
+      'react-hooks/globals': 'error',
+      'react-hooks/immutability': 'error',
+      'react-hooks/preserve-manual-memoization': 'error',
+      'react-hooks/purity': 'error',
+      'react-hooks/refs': 'error',
+      'react-hooks/set-state-in-effect': 'error',
+      'react-hooks/set-state-in-render': 'error',
+      'react-hooks/static-components': 'error',
+      'react-hooks/unsupported-syntax': 'warn',
+      'react-hooks/use-memo': 'error',
+      'react-hooks/incompatible-library': 'warn',
+    },
+  },
 ]);
 
 export default eslintConfig;
